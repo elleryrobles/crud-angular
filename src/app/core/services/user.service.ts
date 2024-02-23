48481d485eee3d0c10b9139c347e79a06ef8e7c4
elleryrobles <elleryrobles@gmail.com>
2024-02-23 12:30:59 -0500

    Ajustando estructura del proyecto

---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, catchError } from 'rxjs';
+import { JwtDto } from '../modelo/jwt-dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  httpClient = inject(HttpClient);
+  authUrl = 'http://localhost:8181/auth';
+
+  postNuevo(formValue: any): Observable<any> {
+    console.log(formValue);
+    return this.httpClient.post<any>(`${this.authUrl}/nuevo`, formValue)
+    .pipe(
+      catchError((error) => {
+        console.error('Error en la solicitud:', error);
+        throw error;
+      })
+    );
+  }
+
+  postLogin(formValue: any): Observable<any> {
+    return this.httpClient.post<any>(`${this.authUrl}/login`, formValue);
+  }
+
+  refresh(dto: JwtDto): Observable<JwtDto> {
+    return this.httpClient.post<JwtDto>(`${this.authUrl}/refresh`, dto);
+  }
+
+  getUsers(): Observable<any> {
+    return this.httpClient.get<any>(`${this.authUrl}/lista`);
+  }
+
+  isLogged(): boolean {
+    return localStorage.getItem('token_crud') ? true : false;
+  }
+}
